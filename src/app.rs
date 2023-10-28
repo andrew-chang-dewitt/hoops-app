dfd1c03fe0fc46f2072408216f6d4ada83e31ee9
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2023-10-28 15:41:27 -0500

    Add basic transaction creation component.

--- src/app.rs
+++ src/app.rs
@@ -46,5 +46,58 @@ fn HomePage() -> impl IntoView {
     view! {
         <h1>"Welcome to Leptos!"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
+        <TransactionCreate />
+    }
+}
+
+/// add Transaction server endpoint
+#[server(prefix = "/api", endpoint = "transaction/create")]
+pub async fn transaction_create(desc: String) -> Result<(), ServerFnError> {
+    println!("{}", desc);
+    Ok(())
+}
+
+/// UI for adding a transaction to the record
+#[component]
+fn TransactionCreate() -> impl IntoView {
+    let transaction_create = create_server_action::<TransactionCreate>();
+
+    view! {
+        <ActionForm action=transaction_create>
+            <Input name="desc".to_string() label="Description:".to_string() />
+            <button type="submit">Create</button>
+        </ActionForm>
+    }
+}
+
+/// Reusable text input component
+#[component]
+pub fn Input(
+    name: String,
+    label: String,
+    #[prop(default = InputType::Text)] input_type: InputType,
+    #[prop(optional)] value: String,
+) -> impl IntoView {
+    let input_type_str: String = input_type.into();
+
+    view! {
+        <label for=&name>{&label}</label>
+        <input id=&name name=&name type=&input_type_str value=&value />
+    }
+}
+
+pub enum InputType {
+    Text,
+    Password,
+    Hidden,
+}
+
+impl Into<String> for InputType {
+    fn into(self) -> String {
+        match self {
+            InputType::Text => String::from("text"),
+            InputType::Password => String::from("password"),
+            InputType::Hidden => String::from("hidden"),
+        }
     }
 }
