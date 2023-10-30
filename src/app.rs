1cababc12c6f286fefed6d9e34eba6859accc7e6
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2023-10-29 20:43:42 -0500

    Add payee and amount to Transaction Creation.

--- src/app.rs
+++ src/app.rs
@@ -39,21 +39,19 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
         <TransactionCreate />
     }
 }
 
 /// add Transaction server endpoint
 #[server(prefix = "/api", endpoint = "transaction/create")]
-pub async fn transaction_create(desc: String) -> Result<(), ServerFnError> {
-    println!("{}", desc);
+pub async fn transaction_create(
+    desc: String,
+    payee: String,
+    amount: f32,
+) -> Result<(), ServerFnError> {
+    println!("{}, {}, {}", desc, payee, amount);
     Ok(())
 }
 
@@ -64,9 +62,24 @@ fn TransactionCreate() -> impl IntoView {
 
     view! {
         <ActionForm action=transaction_create>
+            <Input name="payee".to_string() label="Payee:".to_string() />
             <Input name="desc".to_string() label="Description:".to_string() />
+            <InputAmount name="amount".to_string() label="Amount:".to_string() />
             <button type="submit">Create</button>
         </ActionForm>
+    }
+}
+
+/// Reusable amount input component
+#[component]
+pub fn InputAmount(
+    name: String,
+    label: String,
+    #[prop(optional)] value: String,
+    #[prop(attrs)] attrs: Vec<(&'static str, Attribute)>,
+) -> impl IntoView {
+    view! {
+        <Input {..attrs} name label value input_type=InputType::Number attr:step=0.01 attr:min=0.00 />
     }
 }
 
@@ -77,27 +90,30 @@ pub fn Input(
     label: String,
     #[prop(default = InputType::Text)] input_type: InputType,
     #[prop(optional)] value: String,
+    #[prop(attrs)] attrs: Vec<(&'static str, Attribute)>,
 ) -> impl IntoView {
     let input_type_str: String = input_type.into();
 
     view! {
         <label for=&name>{&label}</label>
-        <input id=&name name=&name type=&input_type_str value=&value />
+        <input {..attrs} id=&name name=&name type=&input_type_str value=&value />
     }
 }
 
 pub enum InputType {
-    Text,
-    Password,
     Hidden,
+    Number,
+    Password,
+    Text,
 }
 
 impl Into<String> for InputType {
     fn into(self) -> String {
         match self {
-            InputType::Text => String::from("text"),
-            InputType::Password => String::from("password"),
             InputType::Hidden => String::from("hidden"),
+            InputType::Number => String::from("number"),
+            InputType::Password => String::from("password"),
+            InputType::Text => String::from("text"),
         }
     }
 }
